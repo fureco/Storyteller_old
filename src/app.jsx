21c37529443b3dc93399b169098db40772f6ed82
Melanie Mende <melanie.mende6@googlemail.com>
2019-07-21 09:52:12 +0200

    top nav bar and tooltips

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
+import {
+  Button,
+  Classes,
+  Menu,
+  MenuItem,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  Popover,
+  Position,
+  Tooltip
+} from '@blueprintjs/core';
+
+import { remote } from 'electron';
+
+const app = remote.app;
+
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <div id="Layout">
+        <Navbar>
+          <NavbarGroup>
+
+            <Tooltip content="Open project" position={Position.BOTTOM}>
+              <Button 
+                className={Classes.MINIMAL} 
+                icon="folder-open" 
+              />
+            </Tooltip>
+
+            <Tooltip content="Quit Storyteller" position={Position.BOTTOM}>
+              <Button
+                className={Classes.MINIMAL}
+                icon="small-cross"
+                onClick={() => app.quit()}
+              />
+            </Tooltip>
+          
+          </NavbarGroup>
+        </Navbar>
+
+        <div id="Main">
+          <h2>Welcome to Storyteller!</h2>
+        </div>
+
+      </div>
+    );
   }
 }
