ff2a3cb117d77aea06d453d5f39b2d706bb1fdda
Melanie Mende <melanie.mende6@googlemail.com>
2019-07-21 10:59:15 +0200

    status bar

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import {
   Alignment,
   Button,
   Classes,
+  Colors,
   Menu,
   MenuItem,
   Navbar,
@@ -19,9 +20,37 @@ import { remote } from 'electron';
 const app = remote.app;
 
 export default class App extends React.Component {
+  
+  constructor() {
+
+    super();
+
+    this.state = {
+      statistic: {
+        words: 0,
+        chars: 0,
+      },
+    };
+
+    this.onInput = this.onInput.bind(this);
+  }
+
+  onInput() {
+    const textField = document.querySelector('#TextField');
+    const textBeforCaret = textField.value.slice(0, textField.selectionStart);
+    const words = textBeforCaret.split(' ');
+    this.setState({ statistic: {
+      words: words.length,
+      chars: textField.value.length,
+    } });
+  }
+
   render() {
     return (
-      <div id="Layout">
+      <div 
+        id="Layout" 
+        style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        
         <Navbar>
           <NavbarGroup align={Alignment.LEFT}>
             <Tooltip content="Open project" position={Position.BOTTOM}>
@@ -43,8 +72,32 @@ export default class App extends React.Component {
           </NavbarGroup>
         </Navbar>
 
-        <div id="Main">
+        <div id="Main" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
           <h2>Welcome to Storyteller!</h2>
+          <textarea
+            id="TextField"
+            style={{
+              height: '100%',
+              flexGrow: 1,
+              overflow: 'auto',
+              border: '1px solid #ddd',
+              resize: 'none',
+              outline: 'none',
+            }}
+            onKeyDown={this.onInput}
+          />
+        </div>
+
+        <div
+          id="StatusBar"
+          style={{
+            backgroundColor: Colors.LIGHT_GRAY5,
+            borderTop: `1px solid ${Colors.LIGHT_GRAY1}`,
+            height: '50px',
+            padding: 12,
+          }}
+        >
+          words: {this.state.statistic.words} - chars: {this.state.statistic.chars}
         </div>
 
       </div>
