9e2d5fafcfaab3c8318ce6b9d91e5a323d4be570
Melanie Mende <melanie.mende6@googlemail.com>
2020-11-03 21:53:54 +0100

    project selector tests

--- src/store/project/project.selectors.js
+++ src/store/project/project.selectors.js
@@ -10,13 +10,9 @@ function getSubRoute(current_route_object) {
 
 		route_string += "/" + current_route_object.current;
 
-		if (current_route_object.current != 'index') {
-			if (current_route_object[current_route_object.current]) {
-				route_string += getSubRoute(current_route_object[current_route_object.current]);
-			}
-		}
-		else {
-			route_string += "/index"
+		if (current_route_object[current_route_object.current]) {
+
+			route_string += getSubRoute(current_route_object[current_route_object.current]);
 		}
 	}
 
