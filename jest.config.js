ecc7f6e03d4a613c1fe4957122726a2f0c73fc83
Melanie Mende <melanie.mende6@googlemail.com>
2020-11-08 14:18:01 +0100

    enzyme.config.js moved into config folder

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
 		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/config/__mocks__/fileMock.js",
 		"\\.(css|less)$": "<rootDir>/config/__mocks__/styleMock.js"
 	},
-    setupFilesAfterEnv: ["<rootDir>enzyme.config.js"]
+    setupFilesAfterEnv: ["<rootDir>/config/enzyme.config.js"]
 };
