4e494276945efa4507b1cbea10cb93dd5f30fa73
Melanie Mende <melanie.mende6@googlemail.com>
2020-11-22 17:50:22 +0100

    fix for no longer loading images

--- src/main.js
+++ src/main.js
@@ -28,7 +28,8 @@ const createWindow = () => {
 		webPreferences: {
 			preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
 			enableRemoteModule: true,
-            nodeIntegration: true,
+			nodeIntegration: true,
+			nodeIntegrationInWorker: true,
             webSecurity: false // to allow copying of local files
         }
      });
@@ -60,6 +61,17 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+import { protocol } from "electron";
+
+app.whenReady().then(() => {
+
+	protocol.registerFileProtocol('file', (request, callback) => {
+		const pathname = decodeURI(request.url.replace('file:///', ''));
+		const parts = pathname.split('?')
+		callback(parts[0]);
+	});
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
